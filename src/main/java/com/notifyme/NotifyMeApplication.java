866d152f04d314973a83e9244aa40cdb10fc9b6c
varuncns <37303602+varuncns@users.noreply.github.com>
2025-06-18 23:34:12 +0530

    Initial project setup: Spring Boot, PostgreSQL, RabbitMQ, Docker

---
+++ src/main/java/com/notifyme/NotifyMeApplication.java
@@ -0,0 +1,13 @@
+package com.notifyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifyMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notifyme/NotifyMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notifyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
