c918338c436e72de2e8bee948740b1823c58c540
varuncns <37303602+varuncns@users.noreply.github.com>
2025-06-19 00:43:44 +0530

    milestone3
    Milestone 3: Email Notification
    	• Integrated Spring Mail
    	• Sent basic plain text emails from the consumer
    
    Milestone 3.1: HTML Email via Thymeleaf
    	• Created email-template.html in resources/templates
    Used SpringTemplateEngine to send rich HTML emails

--- src/main/java/com/notifyme/consumer/NotificationConsumer.java
+++ src/main/java/com/notifyme/consumer/NotificationConsumer.java
@@ -1,18 +1,29 @@
 package com.notifyme.consumer;
 
 import com.notifyme.config.RabbitMQConfig;
-import com.notifyme.dto.NotificationEvent;
+import com.notifyme.dto.NotificationEventDTO;
+import com.notifyme.service.EmailNotificationSender;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class NotificationConsumer {
+	
+	private final EmailNotificationSender emailSender;
 
+	
     @RabbitListener(queues = RabbitMQConfig.NOTIFY_QUEUE)
-    public void consume(NotificationEvent event) {
+    public void consume(NotificationEventDTO event) {
         log.info("Received Notification Event: {}", event);
-        // We will later trigger email sending here (in Milestone 3)
+        
+        if ("EMAIL".equalsIgnoreCase(event.getType())) {
+            emailSender.sendEmail(event);
+        } else {
+            log.warn(" Unsupported notification type: {}", event.getType());
+        }
     }
 }
